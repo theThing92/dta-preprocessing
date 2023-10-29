a587b58da432ad2518f5bcbe9680a8e0821f28ee
Christopher Michael Chandler <christopher.chandler@outlook.de>
2023-10-29 11:51:37 +0100

    Format black Add git ignore

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as XMLTree
 from typing import Dict, Any
 
+
 def load_xml(path_to_xml_file: str) -> XMLTree:
     """TODO
 
@@ -10,6 +11,7 @@ def load_xml(path_to_xml_file: str) -> XMLTree:
          An xml tree object.
     """
     return XMLTree.parse(path_to_xml_file)
+
 
 def get_metadata(xml_tree: XMLTree.ElementTree) -> Dict[str, Any]:
     """TODO
@@ -22,16 +24,18 @@ def get_metadata(xml_tree: XMLTree.ElementTree) -> Dict[str, Any]:
     # relevant information: cf. ressources/dta.txt
     root: XMLTree.Element = xml_tree.getroot()
 
+
 def save_metadata(dict_metadata: Dict[str, Any], output_dir: str) -> None:
     """TODO
 
-        Args:
-            dict_metadata: Dictionary with DTA meta information.
-            output_dir: Path to output directory to save meta data as "csv header".
-        Returns:
-             None
+    Args:
+        dict_metadata: Dictionary with DTA meta information.
+        output_dir: Path to output directory to save meta data as "csv header".
+    Returns:
+         None
     """
     pass
+
 
 if __name__ == "__main__":
     xml_file_path: str = "data/xml/brockes_vergnuegen03_1730.tcf.xml"
