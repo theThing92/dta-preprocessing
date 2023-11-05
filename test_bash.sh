3600f3bf8393fc2e7b97c8d6206e6634033b5a9a
Christopher Michael Chandler <christopher.chandler@outlook.de>
2023-11-05 16:44:57 +0100

    Update test_bash.sh

--- test_bash.sh
+++ test_bash.sh
@@ -1,3 +1,3 @@
-#python preprocessing.py -x data/xml/brockes_vergnuegen03_1730.tcf.xml
+python preprocessing.py -x data/xml/brockes_vergnuegen03_1730.tcf.xml
 python preprocessing.py -s data/xml/brockes_vergnuegen03_1730.tcf.xml
-#python preprocessing.py -x data/xml/brockes_vergnuegen03_1730.tcf.xml -o results
+python preprocessing.py -x data/xml/brockes_vergnuegen03_1730.tcf.xml -o results
